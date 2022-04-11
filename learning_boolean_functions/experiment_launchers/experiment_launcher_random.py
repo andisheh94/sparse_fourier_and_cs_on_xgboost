321079d47ee18c39334be46454f6e04770c96581
andisheh94 <andisheh.amrollahi@gmail.com>
2022-04-11 21:12:37 +0200

    finished random cs launcher

---
+++ learning_boolean_functions/experiment_launchers/experiment_launcher_random.py
@@ -0,0 +1,34 @@
+import os
+from pathlib import Path
+import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser(description='Run the tests for the random sampling approach')
+parser.add_argument('dataset', help='This can be either \'crimes\' or \'superconduct\'')
+parser.add_argument('-n', type=int, default=20)
+parser.add_argument('--notrees', type=int, default=100)
+parser.add_argument('--dryrun', action='store_true')
+args = parser.parse_args()
+dataset, n, no_trees, dry_run = args.dataset, args.n, args.notrees, args.dryrun
+depth_to_mem = {2: 4000, 3: 4000, 4: 4000, 5: 10000}
+depth_to_time = {2: "3:59", 3: "3:59", 4: "23:59", 5: "23:59"}
+for depth in range(2,6):
+    for C in np.linspace(1, 1.6, 10):
+        for ratio in np.linspace(1.1, 2.1, 10):
+            for sampling_factor in np.linspace(0.4,1.5,20):
+                    path = Path(f"../results/random/{dataset}_n={n}_no_trees={no_trees}_depth={depth}_"
+                                f"C={C:.3}_ratio={ratio:.3}_samplefactor={sampling_factor}.json")
+                    if not path.is_file():
+                        submit_string = f"bsub -W {depth_to_time[depth]} "\
+                                        f" -o logs/random/{dataset}_n={n}_no_trees={no_trees}_depth={depth}_" \
+                                        f"C={C:.3}_ratio={ratio:.3}_samplefactor={sampling_factor}.json"\
+                                        f" -R rusage[mem={depth_to_mem[depth]}] "\
+                                        f"python -u reed_solomon_runner.py {n} {no_trees} {depth} {C} {ratio} {sampling_factor} {dataset} "\
+                                        f"&> /dev/null"
+                        if not dry_run:
+                            os.system(submit_string)
+                        print(submit_string)
+
+
+
+
---
+++ learning_boolean_functions/experiment_launchers/random_runner.py
@@ -0,0 +1,35 @@
+from random_forest_builder.random_forest_model import RandomForestModel, Fourier
+from sparse_wht_algorithms.swht_python.sparseWHT_robust_sampleoptimal import SWHTRobust
+import sys
+import numpy as np
+import time
+import json
+if __name__ == "__main__":
+    n, no_trees, depth = [int(sys.argv[j]) for j in [1, 2, 3]]
+    C, ratio, sampling_factor = [float(sys.argv[j]) for j in [4, 5, 6]]
+    dataset = sys.argv[7]
+    np.random.seed(0)
+    random_forest_model = RandomForestModel(dataset, n, no_trees, depth)
+    true_fourier_transform = random_forest_model.get_fourier_transform()
+    k = true_fourier_transform.get_sparsity()
+    # Get Fourier transform
+    # Get Fourier transform again
+    swht = SWHTRobust(n, k, finite_field_class="random_cs", C=C, ratio=ratio, degree=depth, sampling_factor=sampling_factor)
+    start = time.time()
+    fourier_transform = swht.run(true_fourier_transform)
+    end = time.time()
+    elapsed_time = end - start
+    fourier_transform = Fourier.from_tuple_series(fourier_transform)
+    equality = (fourier_transform == true_fourier_transform)
+    mse = Fourier.get_mse(fourier_transform, true_fourier_transform)
+    true_fourier_norm_squared, computed_fourier_norm_squared = true_fourier_transform.norm_squared(), fourier_transform.norm_squared()
+    with open(f"../results/random/{dataset}_n={n}_no_trees={no_trees}_depth={depth}_"
+              f"C={C:.3}_ratio={ratio:.3}_samplefactor={sampling_factor}.json", 'w', encoding='utf-8') as f:
+        results_dict = {"n": n, "no_trees": no_trees, "depth": depth, "C": C, "ratio":ratio,
+                        "sampling_factor": sampling_factor, "k": k, "time": elapsed_time,
+                        "equality": equality, "mse": mse,
+                        "true_fourier_norm_squared": true_fourier_norm_squared,
+                        "computed_fourier_norm_squared": computed_fourier_norm_squared,
+                        "measurements": true_fourier_transform.get_sampling_complexity()}
+        print(results_dict)
+        json.dump(results_dict, f)
