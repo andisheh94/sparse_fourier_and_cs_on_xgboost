c1229949da130b4fc5dfef5bc23515a58b82bc5a
andisheh94 <andisheh.amrollahi@gmail.com>
2022-04-11 23:18:37 +0200

    added different range

--- learning_boolean_functions/experiment_launchers/experiment_launcher_cs.py
+++ learning_boolean_functions/experiment_launchers/experiment_launcher_cs.py
@@ -13,13 +13,13 @@ dataset, n, no_trees, dry_run = args.dataset, args.n, args.notrees, args.dryrun
 depth_to_mem = {2: 4000, 3: 4000, 4: 4000, 5:10000, 6: 40000}
 depth_to_time = {2: "3:59", 3: "3:59", 4: "3:59", 5: "23:59", 6: "23:59"}
 for depth in range(2,7):
-    for C in np.linspace(0.8,1.6,10):
+    for C in np.linspace(0.2, 1, 10):
         for lmda_i, lmda in enumerate(10 ** np.linspace(-4,1,8)):
-            path = Path(f"../results/cs/{dataset}_n={n}_no_trees={no_trees}_depth={depth}"
+            path = Path(f"../results/cs/{dataset}_n={n}_no_trees={no_trees}_depth={depth}_"
                         f"C={C:.3}_lambda={lmda:.6}.json", 'w', encoding='utf-8')
             if not path.is_file():
                 submit_string = f"bsub -W {depth_to_time[depth]} "\
-                                f" -o logs/cs/{dataset}_n={n}_no_trees={no_trees}_depth={depth}_" \
+                                f" -o logs/cs/{dataset}_n={n}_no_trees={no_trees}_depth={depth}" \
                                 f"_C={C:.3}_lambda={lmda:.6}.txt"\
                                 f" -R rusage[mem={depth_to_mem[depth]}] "\
                                 f"python -u cs_runner.py {n} {no_trees} {depth} {C} {lmda} {dataset} "\
