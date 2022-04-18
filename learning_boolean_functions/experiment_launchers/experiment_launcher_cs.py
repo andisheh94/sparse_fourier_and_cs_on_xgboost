46359c7535b5d3bd064988e73e8f6be3b769b8b2
andisheh94 <andisheh.amrollahi@gmail.com>
2022-04-18 14:21:01 +0200

    updated ranges

--- learning_boolean_functions/experiment_launchers/experiment_launcher_cs.py
+++ learning_boolean_functions/experiment_launchers/experiment_launcher_cs.py
@@ -5,21 +5,25 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Run the tests for the proximal (CS) approach')
 parser.add_argument('dataset', help='This can be either \'crimes\' or \'superconduct\'')
-parser.add_argument('-n', type=int, default=20)
-parser.add_argument('--notrees', type=int, default=100)
+parser.add_argument('--notrees', type=int, default=20)
 parser.add_argument('--dryrun', action='store_true')
 args = parser.parse_args()
-dataset, n, no_trees, dry_run = args.dataset, args.n, args.notrees, args.dryrun
-depth_to_mem = {2: 4000, 3: 4000, 4: 4000, 5:10000, 6: 40000}
-depth_to_time = {2: "3:59", 3: "3:59", 4: "3:59", 5: "23:59", 6: "23:59"}
-for depth in range(2,7):
-    for C in np.linspace(0.2, 1, 10):
-        for lmda_i, lmda in enumerate(10 ** np.linspace(-4,1,8)):
+dataset, no_trees, dry_run = args.dataset, args.notrees, args.dryrun
+assert(dataset in ["crimes", "superconduct"])
+if dataset == "crimes":
+    n=500
+elif dataset=="superconduct":
+    n=324
+depth_to_mem = {2: 4000, 3: 10000, 4: 10000, 5: 40000}
+depth_to_time = {2: "3:59", 3: "23:59", 4: "123:59", 5: "123:59"}
+for depth in range(2,6):
+    for C in np.linspace(0.1, 0.9, 10):
+        for lmda_i, lmda in enumerate(10 ** np.linspace(-5,2,8)):
             path = Path(f"../results/cs/{dataset}_n={n}_no_trees={no_trees}_depth={depth}_"
                         f"C={C:.3}_lambda={lmda:.6}.json", 'w', encoding='utf-8')
             if not path.is_file():
                 submit_string = f"bsub -W {depth_to_time[depth]} "\
-                                f" -o logs/cs/{dataset}_n={n}_no_trees={no_trees}_depth={depth}" \
+                                f" -o logs/cs/{dataset}_n={n}_no_trees={no_trees}_depth={depth}.txt" \
                                 f"_C={C:.3}_lambda={lmda:.6}.txt"\
                                 f" -R rusage[mem={depth_to_mem[depth]}] "\
                                 f"python -u cs_runner.py {n} {no_trees} {depth} {C} {lmda} {dataset} "\
