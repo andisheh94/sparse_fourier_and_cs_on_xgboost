907404166f5778878aa865d8fb4010fbe2d37da4
andisheh94 <andisheh.amrollahi@gmail.com>
2022-05-02 18:42:44 +0200

    updated all experiment launchers

--- learning_boolean_functions/experiment_launchers/experiment_launcher_binning.py
+++ learning_boolean_functions/experiment_launchers/experiment_launcher_binning.py
@@ -16,9 +16,9 @@ elif dataset=="superconduct":
     n=324
 depth_to_mem = {2: 1000, 3: 1000, 4: 4000, 5: 4000, 6:4000, 7:10000, 8:10000}
 depth_to_time = {2: "3:59", 3: "3:59", 4: "3:59", 5: "23:59", 6: "23:59", 7: "23:59", 8: "23:59"}
-C_to_ratio = {0.6: [], 0.8: [1.35], 1.0: [2.1]}
-for depth in range(2, 7):
-    for C in [0.6, 0.8, 1.0]:
+C_to_ratio = {0.05: [1.1, 2.6, 5], 0.1: [1.1, 2.6, 5], 0.15: [1.1, 2.6, 5], 0.2: [1.1, 2.6, 5], 0.4: [1.1, 2.6, 5], 0.6: [2.6, 5], 0.8: [2.6, 5]}
+for depth in range(2, 6):
+    for C in [0.05, 0.1, 0.15, 0.2, 0.4, 0.6, 0.8, 1.0]:
         for ratio in C_to_ratio[C]:
             cs_bins_range = np.linspace(0.2 * depth ** 2 , depth ** 2, 10)
             cs_bins_range = [ceil(a) for a in cs_bins_range]
