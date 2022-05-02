4da7904fd5c440ac321a86325472d4fb633384fd
andisheh94 <andisheh.amrollahi@gmail.com>
2022-05-02 15:59:45 +0200

    updated reed-solomon

--- learning_boolean_functions/experiment_launchers/experiment_launcher_reed_solomon.py
+++ learning_boolean_functions/experiment_launchers/experiment_launcher_reed_solomon.py
@@ -18,7 +18,7 @@ depth_to_mem = {2: 1000, 3: 1000, 4: 4000, 5: 4000, 6:4000, 7:10000, 8:10000}
 depth_to_time = {2: "3:59", 3: "3:59", 4: "3:59", 5: "23:59", 6: "23:59", 7: "23:59", 8: "23:59"}
 for depth in range(2, 9):
     for C in np.linspace(0.6, 1.6, 6):
-        for ratio in np.linspace(1.1, 2.1, 5):
+        for ratio in np.linspace(1.1, 2.6, 7):
             path = Path(f"../results/reed_solomon/{dataset}_n={n}_no_trees={no_trees}_depth={depth}_"
                         f"C={C:.3}_ratio={ratio:.3}.json")
             if not path.is_file():
